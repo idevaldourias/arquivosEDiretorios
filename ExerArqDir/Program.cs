a1f21fe0344b8e6dd03aef81a40646a58e4aa7e5
Idevaldo Urias <idevaldorodrigues@gmail.com>
2023-03-24 15:25:49 -0300

    Adicionar arquivos de projeto.

---
+++ ExerArqDir/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace ExerArqDir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\exercicio";
+            string pathOut = path + @"\out";
+            string sourcePath = path + @"\itens.csv";
+            string fileOut = pathOut + @"\summary.csv";
+
+            if (!Directory.Exists(pathOut))
+                Directory.CreateDirectory(pathOut);
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(fileOut))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] values = line.Split(",");
+                        double price = double.Parse(values[1], CultureInfo.InvariantCulture);
+                        int qtd = int.Parse(values[2]);
+
+                        sw.WriteLine(values[0] + "," + (price * qtd).ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.Write("An error has occurred: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
